e4bb4bcfd36e3d1b825774305902614286bc6837
Zee Lavenhouse <Zeehlavenhouse@gmail.com>
2023-03-14 16:55:26 -0400

    completed site then added fucntion during pair program

--- madlibs.py
+++ madlibs.py
@@ -1,6 +1,6 @@
 """A madlib game that compliments its users."""
 
-from random import choice
+from random import choice, sample
 
 from flask import Flask, render_template, request
 
@@ -49,6 +49,38 @@ def greet_person():
     compliment = choice(AWESOMENESS)
 
     return render_template("compliment.html", person=player, compliment=compliment)
+
+@app.route("/game")
+def show_madlib_form():
+    
+    response = request.args.get("no")
+
+    if response == "on":
+        return render_template("goodbye.html")
+    else:
+        return render_template("game.html")
+    
+@app.route("random")
+def random_adj():
+    response = request.args.get("random")
+
+    if response == "on":
+        # random outpout
+    else:
+        return 
+        
+@app.route("/madlib")
+def show_madlib():
+    person = request.args.get("person")
+    color = request.args.get("color")
+    noun = request.args.get("noun")
+    adjective = request.args.get("adjective")
+    adjective1 = sample(adjective, 1)
+    adjective2 = sample(adjective, 1)
+    
+    return render_template("madlib.html", person=person, 
+                           color=color, noun=noun, adjective1=adjective1, adjective2=adjective2)
+
 
 
 if __name__ == "__main__":
